e1d73b3fe921a7d79369d086a7d3d091560a42d0
John E. Tribolet II <john@tribolet.info>
2013-02-15 22:42:57 -0500

    rulers marks and text

--- programarcadegames.com/chapter5.py
+++ programarcadegames.com/chapter5.py
@@ -5,7 +5,7 @@
  
 # Explanation video: http://youtu.be/vRB_983kUMc
  
-import pygame, sys, os, random
+import pygame, sys, os, random, math
 from pygame.locals import *
  
 # Define some colors
@@ -55,13 +55,50 @@ while done==False:
     pygame.draw.line(screen, black, [0,110], [200,100], 5)
     
     for y_offset in range(0, 100, 10):
-        pygame.draw.line(screen, blue, [0, 10 + y_offset], [100, 110 + y_offset], 5)
+        pygame.draw.line(screen, blue, [10, 10 + y_offset], [100, 110 + y_offset], 5)
         
     for y_offset in range(0, 100, 5):
-        pygame.draw.line(screen, blue, [10 + y_offset, 0], [200, 210 + y_offset], 1)
-    
-    for y_offset in range(0, 100, 5):
+        pygame.draw.line(screen, blue, [10 + y_offset, 10], [200, 210 + y_offset], 1)
         pygame.draw.line(screen, red, [10 + y_offset, 100 - y_offset], [400, 410 + y_offset], 1)
+        
+    # ruler marks
+    font = pygame.font.Font(None, 22)
+    for offset in range(0, screen.get_width(), 100):
+        text = font.render(str(offset), True, (255, 0, 255))
+        
+        # horizontal
+        pygame.draw.line(screen, black, [offset, 0], [offset, 5], 4)
+        screen.blit(text, [offset, 0])
+        
+        # vertical
+        pygame.draw.line(screen, black, [0, offset], [5, offset], 4)
+        screen.blit(text, [0, offset])
+        
+    # complex offsets 
+    for i in range(200):   
+        radians_x = i/20
+        radians_y = i/6
+         
+        x = int( 75 * math.sin(radians_x)) + 300
+        y = int( 75 * math.cos(radians_y)) + 200
+         
+        pygame.draw.line(screen,black,[x,y],[x+5,y], 2)
+        
+    # text 
+    # Select the font to use. Default font, 25 pt size.
+    font = pygame.font.Font(None, 25)
+     
+    # Render the text. "True" means anti-aliased text. 
+    # Black is the color. The variable black was defined
+    # above as a list of [0,0,0]
+    # Note: This line creates an image of the letters, 
+    # but does not put it on the screen yet.
+    text = font.render("My text", True, black)
+     
+    # Put the image of the text on the screen at 250x250
+    screen.blit(text, [300,50])
+    
+    
         
     # ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
      
